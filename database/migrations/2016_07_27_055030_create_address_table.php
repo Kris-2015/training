69438cda8911c1e63271b5d655cc63ebc5b2b975
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-31 19:20:41 +0530

    address migration file

---
+++ database/migrations/2016_07_27_055030_create_address_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('type',10);
+            $table->string('street',50);
+            $table->string('city',20);
+            $table->string('state',50);
+            $table->integer('zip');
+            $table->integer('mobile');
+            $table->integer('landline');
+            $table->integer('fax');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('address');
+    }
+}
