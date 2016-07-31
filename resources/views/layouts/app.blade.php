8e0d5d23504e4e791822492d99e74e91d0a3a49a
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-31 19:13:30 +0530

    change in app blade file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,8 +34,22 @@
                     Laravel Training
                 </a>
             </div>
+            <!-- Right side of navbar -->
             <ul class="nav navbar-nav navbar-right">
+            <!-- Authentication Links -->
+            @if(Auth::guest())
                 <li>@yield('page')</li>
+            @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{ Auth::user()->first_name }} <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        <li><a href="{{ url('/list') }}"><i class="fa fa-btn fa-sign-out"></i>Users</a></li>
+                    </ul>
+                </li>
+            @endif
             </ul>
         </div>
     </nav>
