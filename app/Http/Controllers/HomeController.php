8e8ee2cf4d3b296e3bd6055e3d527d42e8059851
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-31 18:58:00 +0530

    home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use App\Models\Address;
+use App\Models\Communication;
 use App\Http\Requests;
+use DB;
 
 class HomeController extends Controller
 {
-    //
+    public function getlist()
+    {
+    	$get_user = DB::table('users')
+    	              ->join('addresses', 'users.id', '=', 'addresses.user_id')
+                      ->join('communications','users.id', '=', 'communications.user_id')
+                      ->groupBy('users.id')
+                      ->get();
+            //->leftJoin('communications', 'users.id', '=', 'communications.user_id')
+            
+    	
+       	$information = $get_user;
+        //dd($information);
+    	return view('list',compact('information'));
+    }
 }
