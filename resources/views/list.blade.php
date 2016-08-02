226e9379020f33d1b59051348001e3d7f0c72e5a
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-02 23:26:44 +0530

    added new columns in list blade

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -2,8 +2,8 @@
 
 @section('title', 'dashboard')
 
-@section('page')
-<a href="/register"><span class="glyphicon glyphicon-user"></span>Registration</a>
+@section('css')
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
 @endsection
 
 @section('content')
@@ -12,29 +12,33 @@
             <table class="table table-bordered" id="user-table">
             	<thead>
             		<tr>
-            			<th>Name</th>
-            			<th>Gender</th>
-            			<th>DOB</th>
-            			<th>Email</th>
-            			<th>Marital Status</th>
-            			<th>Image</th>
-            			<th>City</th>
-            			<th>State</th>
-            			<th>Communication</th>
+            		  <th>Name</th>
+            		  <th>Gender</th>
+            		  <th>DOB</th>
+            		  <th>Email</th>
+            		  <th>Marital Status</th>
+            		  <th>Image</th>
+            		  <th>City</th>
+            		  <th>State</th>
+                      <th>Official State</th>
+                      <th>Official City</th>
+            		  <th>Communication</th>
             		</tr>
             	</thead>
             	<tbody>
             	@foreach($information as $userlist)
           		  <tr>
-          		  	 <td>{{$userlist->first_name . ' ' . $userlist->last_name}}</td>
-          		  	 <td>{{ $userlist->gender }}</td>
-          		  	 <td>{{ $userlist->dob }}</td>
-          		  	 <td>{{ $userlist->email }}</td>
-          		  	 <td>{{ $userlist->marital_status }}</td>
-          		  	 <td><img src='upload/{{$userlist->image}}' width='45' height='45'></td>
-          		  	 <td>{{ $userlist->city }}</td>
-          		  	 <td>{{ $userlist->state }}</td>
-          		  	 <td>{{ $userlist->type }}</td>
+          		    <td>{{$userlist['first_name'] . ' ' . $userlist['last_name']}}</td>
+          		  	<td>{{ $userlist['gender'] }}</td>
+          		  	<td>{{ $userlist['dob'] }}</td>
+          		  	<td>{{ $userlist['email'] }}</td>
+          		  	<td>{{ $userlist['marital_status'] }}</td>
+          		  	<td><img src='upload/{{$userlist['image']}}' width='45' height='45'></td>
+          		  	<td>{{ $userlist['city'] }}</td>
+          		  	<td>{{ $userlist['state'] }}</td>
+                    <td>{{ $userlist['office_city'] }}</td>
+                    <td>{{ $userlist['office_state'] }}</td>
+          		  	<td>{{ $userlist['type'] }}</td>
           		  </tr>
           		@endforeach
           		</tbody>
@@ -42,4 +46,6 @@
         </div>
     </div>
 @endsection
-
+@section('js-css')
+<script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+@endsection
