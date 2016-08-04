c7132d7aa09f942d53cb7aab608844ec900aa32f
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-04 05:47:40 +0530

    permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    /**
+	* table is assosciated with models
+	*
+	* @var String
+	*/
+    $protected $table = 'permissions';
+}
