60b8562caea4b1ce1c7e0a38c4995a300d16dba3
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-04 05:41:17 +0530

    role resource permission migration

---
+++ database/migrations/2016_08_02_190127_role_resource_permission.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RoleResourcePermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_resource_permission', function (Blueprint $table){
+            $table->increments('rrp_id');
+            $table->integer('role_id');
+            $table->integer('resource_id');
+            $table->integer('permission_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('role_resource_permission');
+    }
+}
