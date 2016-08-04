4ac769e35ff2b9319797f8b21c32d058b7f6ca04
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-04 05:32:30 +0530

    role identification added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,22 @@
 @endsection
 
 @section('content')
+	<h2 align="center">Hello, {{ Auth::user()->role_id == 1 ? 'Admin' : 'User' }}</h2>
+
     <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-sm-offset-3 col-sm-5">
             <div class="panel panel-default">
                 <div class="panel-body">
-
-                <h2 align="center">Hello, User</h2>
+                    <ol>
+                      @if(Auth::user()->role_id == 1)
+                       <li><a class="btn btn-primary" href="{{ url('datatables') }}">User Status</a></li><br>
+                       <li><a class="btn btn-info" href="{{ url('list') }}">User</a></li><br>
+                       <li><a class="btn btn-danger" href="{{ url('register') }}">Add New User</a></li>
+                     @else
+                      <li><a class="btn btn-primary" href="{{ url('datatables') }}">User Status</a></li><br>
+                      <li><a class="btn btn-info" href="{{ url('list') }}">User</a></li><br>
+                     @endif
+                    </ol>            
                 </div>
             </div>
         </div>
