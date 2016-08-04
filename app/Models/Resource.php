2322d0ecc70e612ce5d1393bd8c1327826aad4a0
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-04 05:45:00 +0530

    resource model

---
+++ app/Models/Resource.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resource extends Model
+{
+    /**
+	* table is assosciated with models
+	*
+	* @var String
+	*/
+    $protected $table = 'resources';
+}
