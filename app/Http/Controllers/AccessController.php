65b7d62ac6a75acbfa698ecde8754c5bf8a3788c
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-05 09:11:04 +0530

    set permission function added

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -15,39 +15,46 @@ use DB;
 class AccessController extends Controller
 {
    /*
-	 * Display admin panel front end view
-	 *
-	 * @return \Illuminate\View\View
-	*/
+   * Display admin panel front end view
+   *
+   * @return \Illuminate\View\View
+  */
    public function showPanel()
    {
-   		return view('panel');
+      return view('panel');
    }
 
    /*
-	 * Process datatables ajax requests
-	 *
-	 * @return \Illuminate\Http\JsonResponse
+   * Function to get role, resource and permission
+   *
+   * @param handle request
+   * @return \Illuminate\Http\JsonResponse
     */
    public function getrrp(Request $request)
    {
-   	 $flag = $request->all()['start']; 
-   	 if($flag == 1)
-   	 {
-   	 	$data = [];
-   	 	$data['role'] = DB::table('roles')->get();   
-   	 	$data['resource'] = DB::table('resources')->get();
-   	 	$data['permission'] = DB::table('permissions')->get();
+     //flag variable for getting role resource and permission
+     $start = $request->all()['start']; 
+     if($start == 1)
+     {
+      $data = [];
+      $data['role'] = DB::table('roles')->get();   
+      $data['resource'] = DB::table('resources')->get();
+      $data['permission'] = DB::table('permissions')->get();
 
         echo json_encode($data);
-   	 }
+     }
    }
 
-   public function getPerm(Request $request)
+   /*
+   * Function to get permission based on roles
+   *
+   * @param handle request
+   * @return \Illuminate\Http\JsonResponse
+    */
+   public function getPermission(Request $request)
    {
-        //DB::enableQueryLog();
-
-   	    $req = $request->all();
+    
+        $req = $request->all();
         $flag2 = $req['start'];
         $role = $req['role'];
         $resource = $req['resource'];
@@ -66,12 +73,17 @@ class AccessController extends Controller
 
    public function setPermission(Request $request)
    {
+
         $get = $request->all();
-        $flag3 = $get['start'];
+        $temp = $get['start'];
         $role = $get['role'];
         $resource = $get['resource'];
         $permission = $get['permission'];
+        $action = $get['action'];
         
-        dd($get);
+        if($temp == 3)
+        {
+            $setrrp = RoleResourcePermission::addPermission($role, $resource, $permission, $action); 
+        }
    }
 }
