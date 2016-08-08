bbb7d8e17646b2b030dd36bbbafe1cdc174b97c0
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-09 00:26:29 +0530

    Update Buttons resized

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,32 +5,32 @@
 @section('content')
 <h2 align="center">Hello, {{ Auth::user()->role_id == 1 ? 'Admin' : 'User' }}</h2>
 <div class="container">
-   <div class="col-sm-offset-3 col-sm-5">
-      <div class="panel panel-default">
-         <div class="panel-body">
-            @if(session('access'))
-            <div class="alert alert-danger">
-               {{ session('access') }}
+    <div class="col-sm-offset-3 col-sm-5">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                @if(session('access'))
+                <div class="alert alert-danger">
+                    {{ session('access') }}
+                </div>
+                @endif
+                <ol>
+                    @if(Auth::user()->role_id == 1)
+                       <li><a class="btn btn-primary col-xs-4" href="{{ url('datatables') }}">User Status</a></li>
+                       <br>
+                       <li><a class="btn btn-info col-xs-4" href="{{ url('list') }}">User</a></li>
+                       <br>
+                       <li><a class="btn btn-danger col-xs-4" href="{{ url('newUser') }}">Add New User</a></li>
+                       <br>
+                       <li><a class="btn btn-success col-xs-4" href="{{ url('panel') }}">Admin Panel</a></li>
+                    @else
+                       <li><a class="btn btn-primary col-xs-4" href="{{ url('datatables') }}">User Status</a></li>
+                       <br>
+                       <li><a class="btn btn-info col-xs-4" href="{{ url('list') }}">User</a></li>
+                       <br>
+                    @endif
+                </ol>
             </div>
-            @endif
-            <ol>
-               @if(Auth::user()->role_id == 1)
-               <li><a class="btn btn-primary col-xs-4" href="{{ url('datatables') }}">User Status</a></li>
-               <br>
-               <li><a class="btn btn-info col-xs-4" href="{{ url('list') }}">User</a></li>
-               <br>
-               <li><a class="btn btn-danger col-xs-4" href="{{ url('register') }}">Add New User</a></li>
-               <br>
-               <li><a class="btn btn-success col-xs-4" href="{{ url('panel') }}">Admin Panel</a></li>
-               @else
-               <li><a class="btn btn-primary col-xs-4" href="{{ url('datatables') }}">User Status</a></li>
-               <br>
-               <li><a class="btn btn-info col-xs-4" href="{{ url('list') }}">User</a></li>
-               <br>
-               @endif
-            </ol>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
 </div>
 @endsection
