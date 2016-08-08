61282228f9f5a8d5ada7794cfda5afa8a5c191d7
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-09 00:51:56 +0530

    Update modified functions with user information

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -53,6 +53,62 @@ class HomeController extends Controller
     */
     public function getlist()
     {
+        //storing users information
+        $information = $this->UserInformation();
+
+        //returning the users information with list blade file
+        return view('list',compact('information'));
+    }
+
+    /**
+     * Function to get all the data by user id
+     *
+     * @param: integer
+     * @return: void
+    */
+    public function Data($id)
+    {
+        //storing the state list in array
+        $state_list = config('constants.state_list');
+        $users_info = $this->UserInformation();
+
+        return view("registration",compact('users_info','state_list'));
+        
+    }
+
+   /**
+     * Function to delete the user
+     *
+     * @param: id
+     * @return redirect
+    */
+    public function delete(Request $request)
+    {
+        try
+        {   
+            //fetching the user id from request variable
+            $id = $request['id'];
+
+            //performing delete operation
+            User::destroy($id);
+
+            return 1;
+        }
+        catch(Exception $e)
+        {
+            return 0;
+        }
+    }
+
+    /**
+     * Function to fetch all the users information 
+     *
+     * @param: void
+     * @return: array
+    */
+    public function UserInformation()
+    {
+
         $get_user = User::join('addresses', 'users.id', '=', 'addresses.user_id')
                     ->join('communications','users.id', '=', 'communications.user_id')
                     ->groupBy('users.id');
@@ -82,72 +138,6 @@ class HomeController extends Controller
             $information[$key] = $residence + $office;
         }
 
-        return view('list',compact('information'));
-    }
-
-    /**
-     * Function to get all the data by user id
-     *
-     * @param: integer
-     * @return: void
-    */
-    public function Data($id)
-    {
-        $state_list = config('constants.state_list');
-        
-        $get_user = User::join('addresses', 'users.id', '=', 'addresses.user_id')
-                    ->join('communications','users.id', '=', 'communications.user_id')
-                    ->where('users.id',$id);
-        
-        $information_residence = $get_user->where('addresses.type', 'residence')
-                    ->get()->toArray();
-
-        $information_office = User::join('addresses', 'users.id', '=', 'addresses.user_id')
-                    ->select( 
-                        "addresses.street AS office_street",
-                        "addresses.city AS office_city",
-                        "addresses.state AS office_state",
-                        "addresses.zip AS office_zip",
-                        "addresses.mobile AS office_mobile",
-                        "addresses.landline AS office_landline",
-                        "addresses.fax AS office_fax"
-                    )->groupBy('users.id')->where('addresses.type', 'office')
-                    ->get()->toArray();
-        
-        $information = array();
-
-        foreach($information_residence as $key=>$val)
-        { 
-            // Loop though one array
-            $val2 = $information_office[$key]; // Get the values from the other array
-            $information[$key] = $val + $val2; // combine 'em
-        }
-        
-        //dd($information);
-        /*return view("registration",['information' => $information,'state_list' => $state_list ]);*/
-        return view("registration",compact('information','state_list'));
-        
-    }
-
-   /**
-     * Function to delete the user
-     *
-     * @param: id
-     * @return redirect
-    */
-    public function delete(Request $request)
-    {
-        try
-        {
-            
-            $id = $request['id'];
-            User::destroy($id);
-
-            return 1;
-        }
-        catch(Exception $e)
-        {
-            return 0;
-        }
+        return $information;
     }
 }
