382894f1a6bbe32c7cd916ec722ff1f4054ac7e1
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-09 00:59:31 +0530

    Add user account deactivate functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,19 +30,18 @@ class UserController extends Controller
     */
     public function anyData()
     {   
+
         $permission = RoleResourcePermission::datatablePermission(Auth::user()->role_id,'datatables');
         $perm = $permission->name;
 
-        $users = User::select(['id', 'first_name', 'email', 'dob', 'github_id', 'created_at', 'updated_at', 'isActive']);
+        $users = User::withTrashed()->select(['id', 'first_name', 'email', 'dob', 'github_id', 'created_at', 'updated_at', 'isActive', 'deleted_at']);
         $stat = [
-            '0'=>'primary',
-            '1'=>'danger',
-            '2'=>'warning'
+            '0'=>'danger',
+            '1'=>'primary'
         ];
         $status = [
-            '0'=>'Active',
-            '1'=>'Delete',
-            '2'=>'Suspended'
+            '0'=>'Deactivate',
+            '1'=>'Activate'
         ];
 
         return Datatables::of($users,$stat)
@@ -50,46 +49,59 @@ class UserController extends Controller
                     $edit = '';
                     $delete = '';
                     $result='';
-                if($perm == 'all' )
-                {
-                    $edit = '<li><a href="register/'.$users->id.'">Edit</a></li>'; 
-                    $delete = '<li><a href="#" class="delete" data-id="'.$users->id.'">Delete</a></li>';
-                }
-                else
-                    if($perm == 'delete')
-                    {
-                        $delete = '<li><a href="#" class="delete" data-id="'.$users->id.'">Delete</a></li>';
-                    }
-                else
-                    if($perm == 'edit')
+                    if($perm == 'all' )
                     {
                         $edit = '<li><a href="register/'.$users->id.'">Edit</a></li>'; 
+                        $delete = '<li><a href="#" class="delete" data-id="'.$users->id.'">Delete</a></li>';
                     }
+                    else
+                        if($perm == 'delete')
+                        {
+                            $delete = '<li><a href="#" class="delete" data-id="'.$users->id.'">Delete</a></li>';
+                        }
+                    else
+                        if($perm == 'edit')
+                        {
+                            $edit = '<li><a href="register/'.$users->id.'">Edit</a></li>'; 
+                        }
 
-                if($edit != '' || $delete != '' || $perm == 'view')
-                {
-                    $result = '
-                    <div class="btn-group">
-                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Action<span class="caret"></span></button>
-                      <ul class="dropdown-menu" role="menu">
-                        <li><a class="pro" data-id="'.$users->id.'" href="#">Profile</a></li>
-                        <li><a class="git" data-github="'.$users->github_id.'" href="#">Github Profile</a></li>
-                        '.$edit.'
-                        '.$delete.'  
-                      </ul>
-                    </div>';
-                }
-                return $result;
+                    if($edit != '' || $delete != '' || $perm == 'view')
+                    {
+                        $result = '
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Action<span class="caret"></span></button>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a class="pro" data-id="'.$users->id.'" href="#">Profile</a></li>
+                            <li><a class="git" data-github="'.$users->github_id.'" href="#">Github Profile</a></li>
+                            '.$edit.'
+                            '.$delete.'  
+                          </ul>
+                        </div>';
+                    }
+                    return $result;
             })
             ->addColumn('status', function($users) use($stat,$status){
+                $isDeleted = 0;
+
+                if( !empty( $users->deleted_at ) )
+                {
+                    $isDeleted = 1;
+                }
                 return '
-                <button type="button" class="btn btn-'.$stat[$users->isActive].'" btn-lg">'.$status[$users->isActive].'</button>';
+                <button onclick="changeActivationStatus(this)" type="button" data-activate="' . $isDeleted . '" data-id="' . $users->id . '" class="btn btn-'.$stat[$isDeleted].'" btn-lg">'.$status[$isDeleted].'</button>';
             })
             ->make(true);
     }
 
+    /**
+    * Function to get all information for user's profile
+    *
+    * @param: handle incoming ajax request
+    * @return: json
+    */
     public function postUser(Request $request)
     {
+
         if($request->ajax())
         {
             $user_id = $request["id"];
@@ -106,10 +118,18 @@ class UserController extends Controller
         {
             return redirect("/datatables");
         }
+
     }
 
+    /**
+    * Function to communicate with Github api
+    *
+    * @param: handle incoming ajax request
+    * @return: json
+    */
     public function postGit(Request $request)
     {
+
         $github_username = $request["gitid"];
 
         $username = env('GITHUB_USERNAME','kris');
@@ -133,17 +153,22 @@ class UserController extends Controller
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
          ));
+
         $response = curl_exec($handler);
         $err = curl_error($handler);
 
         curl_close($handler);
 
-        if ($err) {
+        if ($err) 
+        {
+
           echo "cURL Error #:" . $err;
           exit;
         } 
+
         header('Content-Type: application/json;charset=utf-8');
         echo $response; 
         exit;
+        
     }
 }
