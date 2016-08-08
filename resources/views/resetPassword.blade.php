62baefa356633ccb2eb02d5d346340b5f74430b8
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-09 01:00:33 +0530

    Add reset password page

--- resources/views/resetPassword.blade.php
+++ resources/views/resetPassword.blade.php
@@ -10,29 +10,27 @@
 <div class="container">
     <div class="col-sm-offset-3 col-sm-5">
         <div class="panel panel-default">
-        	<div class="panel-heading">Reset Password</div>
-        	<div class="panel-body">
-        		{!! Form::open(array('url' => route('resetPassword'), 'method' => 'POST', 'class' => 'form-horizontal','id'=>'login')) !!}
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <!-- Email -->
-                        <div class="form-group">
-                            <label for="email" class="col-sm-3 control-label">Email</label>
-                            <div class="col-sm-8">
-                                {{ Form::email('email', null, array('class'=>'form-control')) }}
-                            </div>
-                        </div> 
-
-                        <!-- Add Task Button -->
-                        <div class="form-group">
-                            <div class="col-sm-offset-4 col-sm-6">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-
-                        {!! Form::close() !!}
-        	</div>
+            <div class="panel-heading">Reset Password</div>
+            <div class="panel-body">
+                {!! Form::open(array('url' => route('resetPassword'), 'method' => 'POST', 'class' => 'form-horizontal','id'=>'login')) !!}
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <!-- Email -->
+                <div class="form-group">
+                    <label for="email" class="col-sm-3 control-label">Email</label>
+                    <div class="col-sm-8">
+                        {{ Form::email('email', null, array('class'=>'form-control')) }}
+                    </div>
+                </div>
+                <!-- Add Task Button -->
+                <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-6">
+                        <button type="submit" class="btn btn-primary">
+                        Send Password Reset Link
+                        </button>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
 </div>
