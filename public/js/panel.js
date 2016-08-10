58de62c8fc86dea853ccf6bc9574756e67647d22
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-10 23:39:00 +0530

    Update Fixed set permission function

--- public/js/panel.js
+++ public/js/panel.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
    });
 
    /*
-    *Set the permission for the resource
+    *Set the permission for the resource on the basis of role
     */
    $(document).on('change', '.privilege input[type="checkbox"]', function() {
 
@@ -58,19 +58,23 @@ function display() {
 
          for (var key in data.role) {
             if (data.role.hasOwnProperty(key)) {
-               role += '<option id="' + data.role[key]['role_name'] + '" value="' + data.role[key]['role_id'] + '">' + data.role[key]['role_name'] + '</option>';
+               role += '<option id="' + data.role[key]['role_name'] + '" value="' + 
+               data.role[key]['role_id'] + '">' + data.role[key]['role_name'] + '</option>';
             }
          }
 
          for (var key in data.resource) {
             if (data.resource.hasOwnProperty(key)) {
-               resource += '<option id="' + data.resource[key]['resource_name'] + '" value="' + data.resource[key]['resource_id'] + '">' + data.resource[key]['resource_name'] + '</option>';
+               resource += '<option id="' + data.resource[key]['resource_name'] + '" value="' 
+               + data.resource[key]['resource_id'] + '">' + data.resource[key]['resource_name'] + '</option>';
             }
          }
 
          for (var key in data.permission) {
             if (data.permission.hasOwnProperty(key)) {
-               privilege += '<label class="checkbox-inline"><input type="checkbox" id="' + data.permission[key]['name'] + '" value="' + data.permission[key]['permission_id'] + '">' + data.permission[key]['name'] + '</label>';
+               privilege += '<label class="checkbox-inline"><input type="checkbox" id="' + 
+               data.permission[key]['name'] + '" value="' + data.permission[key]['permission_id'] + '">' 
+               + data.permission[key]['name'] + '</label>';
             }
          }
 
@@ -82,7 +86,7 @@ function display() {
 }
 
 /**
- *function to select the privilege checkbox
+ * Function to select the privilege checkbox
  * @param: role_id and resource_id
  * @return: void
  */
@@ -120,8 +124,8 @@ function sendrrp(get_role, get_resource) {
    });
 }
 
-/*
- *function to set permission
+/**
+ * Function to set permission
  * @param: role_id,resource_id,privilege_id, action
  * @return: void
  */
