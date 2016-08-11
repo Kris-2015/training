c2742e07a7ee38773c0aa268a9c35f79a5fc2bb4
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-11 10:13:42 +0530

    Update aligned the html tags

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -9,31 +9,31 @@
 @section('content')
 <!-- Admin Panel -->
 <div class="container">
-   <div class="row">
-      <div class="col-xs-offset-2 col-xs-7">
-         <div class="panel panel-default">
-            <div class="panel-heading">
-               <p align="center"><strong>ADMIN PANEL</strong></p>
+    <div class="row">
+        <div class="col-xs-offset-2 col-xs-7">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <p align="center"><strong>ADMIN PANEL</strong></p>
+                </div>
+                <div class="panel-body">
+                    <form class="form-inline" role="form">
+                        <div class="form-group">
+                            <label for="role">Role:</label>
+                            <select class="form-control role" name="role">
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="resource">Resource:</label>
+                            <select class="form-control resource" name="resource">
+                            </select>
+                        </div>
+                        <div class="form-group privilege">
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="panel-body">
-               <form class="form-inline" role="form">
-                  <div class="form-group">
-                     <label for="role">Role:</label>
-                     <select class="form-control role" name="role">
-                     </select>
-                  </div>
-                  <div class="form-group">
-                     <label for="resource">Resource:</label>
-                     <select class="form-control resource" name="resource">
-                     </select>
-                  </div>
-                  <div class="form-group privilege">
-                  </div>
-               </form>
-            </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
 </div>
 @endsection
 
