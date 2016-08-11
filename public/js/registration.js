3e285fa7c81bf783635b93d5910e2abedc16eb61
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-11 10:22:26 +0530

    Update modified regex for email validation

--- public/js/registration.js
+++ public/js/registration.js
@@ -126,7 +126,7 @@ function validatePassword()
 function validateEmail()
 {
     $("#email").on('focus keypress keydown blur', function(){
-        //var mail_exp =/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]$/;//{2,4}
+        
         var mail = $(this).val().trim();
 
         if(mail == "")
