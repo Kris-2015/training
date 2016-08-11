dd54cec74fc9f7acd0ed81f4115a94c073872aa3
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-11 10:16:10 +0530

    Update fixed url issue

--- resources/views/emails/reset.blade.php
+++ resources/views/emails/reset.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <h4 align="center">Welcome to Laravel Training </h4>
-<p>Click on <a href="url{{ 'reset/$key' }}">{{ $key }}</a> to change your password</p>
+<p>Click on <a href="{{ url('/reset',$key) }}">{{ $key }}</a> to change your password</p>{{ url('/reset',$key) }}
 @endsection
