9d7ab2cfc193000c1b3061e3a086bfca220f1d0a
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-12 19:25:26 +0530

    Update added ajax header function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,7 +77,13 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-   <!--  <script src="//code.jquery.com/jquery-1.10.2.min.js"></script> -->
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
     @yield('js-css')
     @stack('scripts')
 </body>
