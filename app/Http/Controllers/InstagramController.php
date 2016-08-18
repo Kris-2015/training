aa422d969b6782f13b90d0eaa0dc05c6f4c88e25
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-18 11:42:17 +0530

    Update added sepearate function for curl

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -21,15 +21,6 @@ use Vinkla\Instagram\InstagramManager;
 class InstagramController extends Controller
 {
 
-    //taking a protected variable of instagram
-    protected $instagram;
-
-
-    public function __construct(InstagramManager $instagram)
-    {
-        $this->instagram = $instagram;
-    }
-
     /**
     * Function to interact with Instagram api for user sign up
     *
@@ -38,32 +29,13 @@ class InstagramController extends Controller
     */
     public function details(Request $request)
     {
-
         //get the response code given instagram api
         $code = $request->all()['code'];
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,'https://api.instagram.com/oauth/access_token');
-        curl_setopt($ch, CURLOPT_POST, 1);
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-        'client_id' => env('CLIENT_ID'),
-        'client_secret' => env('CLIENT_SECRET') ,
-        'grant_type' => 'authorization_code',
-        'redirect_uri' => env('REDIRECT_URI'),
-        'code' => $code
-        )));
-
-        // receive server response ...
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $server_response = curl_exec ($ch);
-
-        $server_response_json = json_decode($server_response);
         
-        curl_close ($ch);
-        
-        return $this->homePage($server_response_json);
+        //instagram data
+        $insta_data = $this->curlInsta();
+
+        return $this->homePage($insta_data);
     } 
 
    /**
@@ -88,7 +60,7 @@ class InstagramController extends Controller
         $id = User::where('instagram_username', $username)->value('id');
 
         //check if the user is present in database
-        if(!isset($id))//
+        if(!isset($id))
         {
             //user does not exist, creating a new user
             $insta_userdata = array(
@@ -121,6 +93,38 @@ class InstagramController extends Controller
             //if login operation failed, direct the user to login page
             return redirect('login')->with('warning', 'Something went wrong.');
         }
-  
    }
+
+  /**
+    * Function to perform curl operation for instagram api
+    * 
+    * @param void
+    * @return json
+   */
+    public function curlInsta()
+    {
+        //initialising the curl operation
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,'https://api.instagram.com/oauth/access_token');
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+            'client_id' => env('CLIENT_ID'),
+            'client_secret' => env('CLIENT_SECRET') ,
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => env('REDIRECT_URI'),
+            'code' => $code
+        )));
+
+        // receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_response = curl_exec ($ch);
+
+        $server_response_json = json_decode($server_response);
+
+        curl_close ($ch);
+
+        return $server_response_json;
+    }
 }
