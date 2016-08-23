e35ef6868403f5d2e9f5d5fea56d782f758492ef
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-24 00:22:35 +0530

    Add route to upload file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -85,6 +85,11 @@ Route::post('activate', 'ActivateUserController@activateUser');
 Route::get('/redirect', 'SocialAuthController@redirect');
 Route::get('/callback', 'SocialAuthController@callback');
 
+
+Route::post('image', function() {
+    return 'hello';
+});
+
 /**
  * Group routes to check the user authentication
 */
