e5a7b7176919af2b42e022fc1f85c12d9daed832
Kris-2015 <krishnadev.rnc@gmail.com>
2016-08-25 11:52:30 +0530

    Fix indentation issue

--- app/Models/UserActivation.php
+++ app/Models/UserActivation.php
@@ -28,21 +28,21 @@ class UserActivation extends Model
     {
         try
         {
-             DB::beginTransaction();
-             $insert_activation_code = new UserActivation();
+            DB::beginTransaction();
+            $insert_activation_code = new UserActivation();
            
-             $insert_activation_code->user_id = $data['user_id'];
-             $insert_activation_code->token = $data['token'];
+            $insert_activation_code->user_id = $data['user_id'];
+            $insert_activation_code->token = $data['token'];
 
-             $sucess_insertcode = $insert_activation_code->save();
+            $sucess_insertcode = $insert_activation_code->save();
 
-             if($sucess_insertcode == 1)
-             {
-                DB::commit();
-                return 1;
-             }
+            if($sucess_insertcode == 1)
+            {
+               DB::commit();
+               return 1;
+            }
 
-             throw new Exception("Error: Failed to process activation key");
+            throw new Exception("Error: Failed to process activation key");
         }
         catch(Exception $e)
         {
