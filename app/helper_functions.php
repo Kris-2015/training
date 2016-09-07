72d925e611ba3418378a4a7ec13ef591cddcc2f6
Kris-2015 <krishnadev.rnc@gmail.com>
2016-09-07 08:51:56 +0530

    Add function header

--- app/helper_functions.php
+++ app/helper_functions.php
@@ -1,6 +1,12 @@
 <?php
  use Log;
-   
+ 
+ /**
+  * Function is used for versioning the css and js files
+  *
+  * @param path of css and js file
+  * @return updated version of files
+ */   
 function asset_timed($path)
 {
 
@@ -10,6 +16,12 @@ function asset_timed($path)
     return $return_path . '?' . $timestamp;
 }
 
+ /**
+  * Function is used for logging error
+  *
+  * @param error message
+  * @return void
+ */
 function errorReporting($error)
 {
 	Log::error($error);
