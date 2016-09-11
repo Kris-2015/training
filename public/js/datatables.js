9b450e68ae693c5cb360cc81f0b5c4448d994a4b
Kris-2015 <krishnadev.rnc@gmail.com>
2016-09-11 21:18:39 +0530

    Fix alias issue

--- public/js/datatables.js
+++ public/js/datatables.js
@@ -21,8 +21,8 @@ $(document).ready(function() {
             { data: 'first_name', name: 'first_name' },
             { data: 'email', name: 'email' },
             { data: 'dob', name:'dob'},
-            { data: 'created_at', name:'created_at'},
-            { data: 'updated_at', name:'updated_at'},
+            { data: 'created_at', name:'users.created_at'},
+            { data: 'updated_at', name:'users.updated_at'},
             { data: 'action', name: 'action', orderable: false, searchable: false},
             { data: 'status', name: 'status', orderable:false, searchable: false}
         ]
